1f6f1d228b9f584c367d0f4fece732b0a15c705b
Arnold <Arnold>
2026-09-14 12:20:28 +0300

    added navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 function Navbar() {
   return (
-    <div>
+    <div className='navbar'>
+      <div className='logo'>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+      </div>
+      <div className='links'> 
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <Link to="/lms">LMS</Link>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Lms from './pages/Lms.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  { 
+    path: "/lms",
+    element: <Lms />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />, 
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )

5fa95300970fda97aeb0f87a26f555b157b14c0f
Arnold <Arnold>
2026-09-15 17:39:57 +0300

    added hero text and hero image

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import img1 from './assets/Girlreadingbook.png'
 
 
 
@@ -12,7 +13,20 @@ function App() {
 
   return (
     <>
+    <div className="hero">
       <Navbar />
+      <div className="hero-content">
+       <div className="hero-text">
+        <h1>Welcome to</h1>
+        <h1>Learn </h1>
+        <p>Learn and grow with our Learning Management System.</p>
+       </div>
+       <div className="hero-image">
+         <img src={img1} alt="Hero" />
+       </div>
+      </div>
+    </div>
+
     </>
   )
 }
